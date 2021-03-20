ce41eb247dac1611fb7664a90dfc9e36251e6820
alkxyly <alkxyly@hotmail.com>
2021-03-20 20:25:35 -0300

    Configurando Dados para o banco de dados com import.sql

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -13,7 +13,7 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "nom_cozinha", length = 30)
+	@Column(length = 30)
 	private String nome;
 
 	public Long getId() {
