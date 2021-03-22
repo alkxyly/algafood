0afe246a0a0129982158510d46bc8272bffff8c4
alkxyly <alkxyly@hotmail.com>
2021-03-21 21:22:35 -0300

    Relacionamento ManyToOne

--- src/main/java/com/algaworks/algafood/jpa/BuscarRestauranteMain.java
+++ src/main/java/com/algaworks/algafood/jpa/BuscarRestauranteMain.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.jpa;
 
+import java.util.List;
+
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -16,8 +18,11 @@ public class BuscarRestauranteMain {
 				.run(args);
 	
 		RestauranteRepository restauranteRepository = applicationContext.getBean(RestauranteRepository.class);
-		Restaurante restaurante = restauranteRepository.porId(1L);
-		System.out.println(restaurante.toString());
+		List<Restaurante> todosRestaurantes = restauranteRepository.todas();
+		todosRestaurantes.forEach(r -> {
+			System.out.printf("%s - %f- %s",r.getNome(), r.getTaxaFrete(), r.getCozinha().getNome());
+			System.out.println("");
+		});
 		
 	}
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert into cozinha (nome) values ('Tailandesa');
-insert into cozinha (nome) values ('Indiana');
+insert into cozinha (id, nome) values (1, 'Tailandesa');
+insert into cozinha (id, nome) values (2, 'Indiana');
 
-insert into restaurante (nome, taxa_frete) values ('Restaurante 1', 10.50);
-insert into restaurante (nome, taxa_frete) values ('Restaurante 2', 9.90);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Restaurante 1', 10.50, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Restaurante 2', 9.90, 2);
