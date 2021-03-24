a01e17ae7a0bb652b4ee075d6f07a6b6daf02df3
alkxyly <alkxyly@hotmail.com>
2021-03-24 16:12:41 -0300

    4.30. Modelando e implementando a inclusão de recursos de restaurantes

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -4,21 +4,26 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
-import com.algaworks.algafood.domain.service.RestauranteService;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
 
 	@Autowired
-	private RestauranteService restauranteService;	
+	private CadastroRestauranteService restauranteService;	
 
 	@GetMapping
 	public List<Restaurante> listar(){
@@ -32,6 +37,19 @@ public class RestauranteController {
 			return ResponseEntity.ok(restaurante);
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante){
+		try {
+			 restaurante = restauranteService.salvar(restaurante);
+			 
+			 return ResponseEntity.status(HttpStatus.CREATED)
+					 .body(restaurante);
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
 	}
 
 }
---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,41 @@
+package com.algaworks.algafood.domain.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+@Service
+public class CadastroRestauranteService {
+	
+	
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
+	@Autowired 
+	private CozinhaRepository cozinhaRepository;
+	
+	public List<Restaurante> listar(){
+		return restauranteRepository.listar();
+	}
+	
+	public Restaurante buscar(Long id) {
+		return restauranteRepository.buscar(id);
+	}
+	
+	public Restaurante salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		if(cozinha == null) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe cadastro de cozinha com código %d", cozinhaId));
+		}
+		return restauranteRepository.salvar(restaurante);
+	}
+}
