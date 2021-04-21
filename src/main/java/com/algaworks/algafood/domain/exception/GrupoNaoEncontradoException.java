060f03900b83b7e521ad8dcb78b3424c85959ffa
alkxyly <alkxyly@hotmail.com>
2021-04-20 21:25:22 -0300

    12.8. Desafio: implementando os endpoints de grupos

---
+++ src/main/java/com/algaworks/algafood/domain/exception/GrupoNaoEncontradoException.java
@@ -0,0 +1,14 @@
+package com.algaworks.algafood.domain.exception;
+
+public class GrupoNaoEncontradoException extends EntidadeNaoEncontradaException{
+
+	private static final long serialVersionUID = 1L;
+
+	public GrupoNaoEncontradoException(String mensagem) {
+		super(mensagem);
+	}
+	
+	public GrupoNaoEncontradoException(Long grupoId) {
+		this(String.format("Grupo com código %d não encontrado", grupoId));
+	}
+}
