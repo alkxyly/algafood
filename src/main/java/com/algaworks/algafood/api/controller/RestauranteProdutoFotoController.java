40e8bd68863e38e447accc4a0b1ac5c80c4272cf
alkxyly <alkxyly@hotmail.com>
2021-05-21 22:24:57 -0300

    14.12. Desafio: Implementando endpoint de consulta de foto de produto

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,5 +53,12 @@ public class RestauranteProdutoFotoController {
 		FotoProduto fotoSalva = catalogoFotoProdutoService.salvar(foto, arquivo.getInputStream());
 		
 		return fotoProdutoModelAssembler.toModel(fotoSalva);
+	}
+	
+	@GetMapping
+	public FotoProdutoModel buscar(@PathVariable Long restauranteId,@PathVariable Long produtoId) {
+		FotoProduto foto = catalogoFotoProdutoService.buscarOuFalhar(restauranteId, produtoId);
+		return fotoProdutoModelAssembler.toModel(foto);
+		
 	}
 }
