689a51ede12c1baa1370282aa963fc746ba9efda
alkxyly <alkxyly@hotmail.com>
2021-05-30 02:34:51 -0300

    16.3. Entendendo o funcionamento básico de CORS e habilitando na API

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -6,8 +6,9 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,15 +48,21 @@ public class RestauranteController {
 	
 	@GetMapping
 	@JsonView(RestauranteView.Resumo.class)
-	public List<RestauranteModel> listar(){
-		return restauranteModelAssembler.toCollectionModel(restauranteService.listar());
+	public ResponseEntity<List<RestauranteModel>> listar(){
+		
+		List<RestauranteModel> restaurantesModel = restauranteModelAssembler
+				.toCollectionModel(restauranteService.listar());
+		
+		return ResponseEntity.ok()
+				.header(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+				.body(restaurantesModel);
 	}
 	
-	@JsonView(RestauranteView.ApenasNome.class)
-	@GetMapping(params = "projecao=apenas-nome")
-	public List<RestauranteModel> listarApenasNome(){
-		return restauranteModelAssembler.toCollectionModel(restauranteService.listar());
-	}
+//	@JsonView(RestauranteView.ApenasNome.class)
+//	@GetMapping(params = "projecao=apenas-nome")
+//	public List<RestauranteModel> listarApenasNome(){
+//		return restauranteModelAssembler.toCollectionModel(restauranteService.listar());
+//	}
 	
 //	@GetMapping
 //	public MappingJacksonValue listar(@RequestParam(required = false) String projecao){
--- src/main/resources/cors/client.js
+++ src/main/resources/cors/client.js
@@ -1,10 +1,10 @@
 function consultarRestaurantes() {
   $.ajax({
-    url: "http://api.algafood.local:8080/restaurantes",
+    url: "http://localhost:8080/restaurantes",
     type: "get",
 
     success: function(response) {
-      $("#conteudo").text(response);
+      $("#conteudo").text(JSON.stringify(response));
     }
   });
 }
