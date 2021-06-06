f5a82f8344c946ece0b6f595f4a4ffe4fbf0c2e1
alkxyly <alkxyly@hotmail.com>
2021-06-06 01:25:52 -0300

    17.10. Desafio: implementando requisições condicionais com Deep ETags

--- src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -72,14 +72,32 @@ public class FormaPagamentoController {
 	}
 	
 	@GetMapping("/{formaPagamentoId}")
-	public ResponseEntity<FormaPagamentoModel> buscar(@PathVariable Long formaPagamentoId) {
-	  FormaPagamento formaPagamento = cadastroFormaPagamentoService.buscarOuFalhar(formaPagamentoId);
-	  
-	  FormaPagamentoModel formaPagamentoModel =  formaPagamentoAssembler.toModel(formaPagamento);
-	  
-	  return ResponseEntity.ok()
-	      .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
-	      .body(formaPagamentoModel);
+	public ResponseEntity<FormaPagamentoModel> buscar(@PathVariable Long formaPagamentoId,
+	        ServletWebRequest request) {
+	    
+	    ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+	    
+	    String eTag = "0";
+	    
+	    OffsetDateTime dataAtualizacao = formaPagamentoRepository
+	            .getDataAtualizacaoById(formaPagamentoId);
+	    
+	    if (dataAtualizacao != null) {
+	        eTag = String.valueOf(dataAtualizacao.toEpochSecond());
+	    }
+	    
+	    if (request.checkNotModified(eTag)) {
+	        return null;
+	    }
+	    
+	    FormaPagamento formaPagamento = cadastroFormaPagamentoService.buscarOuFalhar(formaPagamentoId);
+	    
+	    FormaPagamentoModel formaPagamentoModel = formaPagamentoAssembler.toModel(formaPagamento);
+	    
+	    return ResponseEntity.ok()
+	            .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+	            .eTag(eTag)
+	            .body(formaPagamentoModel);
 	}
 	
 	@PostMapping
