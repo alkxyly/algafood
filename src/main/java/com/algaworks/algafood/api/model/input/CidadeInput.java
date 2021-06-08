2cb7b0ca0125970cd201dba022652c1d4a7fa0a7
alkxyly <alkxyly@hotmail.com>
2021-06-08 00:12:38 -0300

    18.11. Descrevendo restrições de validação de propriedades do modelo

--- src/main/java/com/algaworks/algafood/api/model/input/CidadeInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/CidadeInput.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 @Setter
 public class CidadeInput {
 	
-	@ApiModelProperty(example = "Aracaju")
+	@ApiModelProperty(example = "Aracaju", required = true)
 	@NotBlank
 	private String nome;
 	
