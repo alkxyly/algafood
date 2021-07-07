e1790ab175f64cfad91a0a9e18308fb2aa42a73c
alkxyly <alkxyly@hotmail.com>
2021-07-06 22:41:06 -0300

    19.9. Construindo links que apontam para métodos

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -1,12 +1,14 @@
 package com.algaworks.algafood.api.controller;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -55,15 +57,16 @@ public class CidadeController implements CidadeControllerOpenApi{
 		var cidade = cadastroCidade.buscarOuFalhar(cidadeId);
 		CidadeModel cidadeModel = cidadeModelAssembler.toModel(cidade);
 		
-		cidadeModel.add(linkTo(CidadeController.class)
-					.slash(cidadeModel.getId())
-					.withSelfRel());
+		Link link = linkTo(methodOn(CidadeController.class)
+				.buscar(cidadeModel.getId())).withSelfRel();
 		
-		cidadeModel.add(linkTo(CidadeController.class).withRel("cidades"));
+		cidadeModel.add(link);
 		
-		cidadeModel.getEstado().add(linkTo(EstadoController.class)
-				.slash(cidadeModel.getEstado().getId())
-				.withSelfRel());
+		cidadeModel.add(linkTo(methodOn(CidadeController.class)
+				.listar()).withRel("cidades"));
+		
+		cidadeModel.getEstado().add(linkTo(methodOn(EstadoController.class)
+				.buscar(cidadeModel.getEstado().getId())).withSelfRel());
 		
 		return cidadeModel; 
 	}
