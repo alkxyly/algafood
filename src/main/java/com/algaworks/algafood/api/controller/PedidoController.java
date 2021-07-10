95a9fa5148d2b25a15b1837e80cdca0f5106e04e
alkxyly <alkxyly@hotmail.com>
2021-07-10 19:35:10 -0300

    19.16. Desafio: adicionando hypermedia em recursos de pedidos (paginação)

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -1,12 +1,11 @@
 package com.algaworks.algafood.api.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -57,21 +56,24 @@ public class PedidoController implements PedidoControllerOpenApi {
     @Autowired
     private PedidoInputDisassembler pedidoInputDisassembler;
     
+    @Autowired
+    private PagedResourcesAssembler<Pedido> pagedResourcesAssembler;
+    
     @ApiImplicitParams({
     	@ApiImplicitParam(value = "Nomes das propriedades para filtrar na resposta, separados por vírgula",
     			name = "campos", paramType = "query", type = "string")
     })
+
+    @Override
     @GetMapping
-    public Page<PedidoResumoModel> pesquisar(PedidoFilter filtro, 
-    		@PageableDefault(size = 10) Pageable pageable) {
-     
-    	pageable = traduzirPageable(pageable);
-    	
-    	Page<Pedido> pedidosPage = pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro),
-    			pageable);
-        List<PedidoResumoModel> pedidoResumoModel =  pedidoResumoModelAssembler.toCollectionModel(pedidosPage.getContent());
-        Page<PedidoResumoModel> pedidoResumoModelPage = new PageImpl<>(pedidoResumoModel, pageable, pedidosPage.getTotalElements());
-        return pedidoResumoModelPage;
+    public PagedModel<PedidoResumoModel> pesquisar(PedidoFilter filtro, 
+            @PageableDefault(size = 10) Pageable pageable) {
+        pageable = traduzirPageable(pageable);
+        
+        Page<Pedido> pedidosPage = pedidoRepository.findAll(
+                PedidoSpecs.usandoFiltro(filtro), pageable);
+        
+        return pagedResourcesAssembler.toModel(pedidosPage, pedidoResumoModelAssembler);
     }
     
     @ApiImplicitParams({
