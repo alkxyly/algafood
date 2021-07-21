53e1106d4c034d93e3127067c172cf8ad1d7a710
Alkxyly <alkxyly@hotmail.com>
2021-07-20 21:39:25 -0300

    19.29. Adicionando links com template variable de caminho de formas de pagamento do restaurante

--- src/main/java/com/algaworks/algafood/api/AlgaLinks.java
+++ src/main/java/com/algaworks/algafood/api/AlgaLinks.java
@@ -185,7 +185,13 @@ public class AlgaLinks {
 		return linkTo(methodOn(RestauranteFormaPagamentoController.class)
 				.desassociar(restauranteId, formaPagamentoId))
 				.withRel(rel);
+	}
+	
+	public Link linkToRestauranteFormaPagamentoAssociacao(Long restauranteId, String rel) {
 		
+		return linkTo(methodOn(RestauranteFormaPagamentoController.class)
+				.associar(restauranteId, null))
+				.withRel(rel);
 	}
 
 	public Link linkToCozinha(Long cozinhaId, String rel) {
