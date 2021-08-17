db52c3049558abd2e5b674af8730701992881f61
alkxyly <alkxyly@hotmail.com>
2021-08-17 15:49:58 -0300

    23.25. Carregando Granted Authorities dos escopos do OAuth2 no Resource Server

--- src/main/java/com/algaworks/algafood/api/v1/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/CozinhaController.java
@@ -10,6 +10,7 @@ import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -55,6 +56,8 @@ public class CozinhaController implements CozinhaControllerOpenApi{
 	@CheckSecurity.Cozinhas.PodeConsultar
 	@GetMapping
 	public PagedModel<CozinhaModel> listar(@PageableDefault(size = 10) Pageable pageable){
+		System.out.println(SecurityContextHolder.getContext().getAuthentication().getAuthorities());
+		
 		log.info("Consultando Cozinhas com paginas de {} registros", pageable.getPageSize());
 		
 		Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,16 +1,18 @@
 package com.algaworks.algafood.core.security;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,12 +22,6 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-//			.authorizeRequests()
-//				.antMatchers(HttpMethod.POST, "/v1/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
-//				.antMatchers(HttpMethod.PUT, "/v1/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
-//				.antMatchers(HttpMethod.GET, "/v1/cozinhas/**").authenticated()
-//				.anyRequest().denyAll()
-//			.and()
 				.csrf().disable()
 				.cors()
 			.and()
@@ -41,10 +37,17 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			if(authorities == null) 
 				return Collections.emptyList();
 			
-			return authorities.stream()
+			var scopesAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+			Collection<GrantedAuthority> grantedAuthorities = scopesAuthoritiesConverter
+					.convert(jwt); 
+			
+			grantedAuthorities.addAll( authorities.stream()
 					.map(SimpleGrantedAuthority::new)
-					.collect(Collectors.toList());
+					.collect(Collectors.toList()));
+			
+			return  grantedAuthorities;
 		});
+		
 		return jwtAuthenticationConverter;
 	}
 	
